efd097e9091bfb1b4dc235d724585d365af033d9
aktweb <aktweb.0@gmail.com>
2024-02-17 21:41:59 +0530

    test

--- app/item/quick-release-couplers/page.tsx
+++ app/item/quick-release-couplers/page.tsx
@@ -2,11 +2,12 @@ import Image from "next/image";
 const AirCylinders = () => {
   return (
     <div className=" flex-col space-y-10">
-      <div className=" flex justify-center font-semibold text-xl">
+      <div className=" flex justify-center font-bold text-5xl text-slate-700">
+        <br/>
         Quick Release Couplers (QRCs)
       </div>
 
-      <div className=" p-2 font-bold text-2xl">
+      <div className="  p-2 font-semibold text-4xl">
         1. Pneumatic Quick Release Couplers
       </div>
       <div className=" block text-center items-start">
@@ -14,41 +15,42 @@ const AirCylinders = () => {
           alt=""
           className=" float-left  w1/2 me-4 ms-4"
           src={`/1. Pneumatic Quick Release Couplers.jpg`}
-          height={450}
-          width={450}
+          height={350}
+          width={350}
         ></Image>
         <div className=" text-justify">
-          <p className="font-semibold text-2xl ">Features</p>
-          <p className=" font-semibold text-xl mt-2"> i. Uni-Directional:</p>
+          <p className="font-semibold text-2xl text-gray-500  ">Features</p>
+          <p className=" font-semibold text-xl mt-2 "> Uni-Directional:</p>
           <span className=" text-l">
             Uni-directional shut-off coupler with an automatic shut-off valve
             built in the socket.
           </span>
-          <p className=" font-semibold text-xl mt-2">ii. General Purpose:</p>
+          <p className=" font-semibold text-xl mt-2 transform skew-x-12">General Purpose:</p>
           <span className=" text-l">
             An excellent general purpose coupler for connecting factory air
             supply to pneumatic tool hose.
           </span>
-          <p className=" font-semibold text-xl mt-2">iii. Structural Parts:</p>
+          <p className=" font-semibold text-xl mt-2 transform skew-x-12"> Structural Parts:</p>
           <span className=" text-l">
             Important structural parts of steel models are hardened for added
             strength. Extremely wear resistant and durable.
           </span>
-          <p className=" font-semibold text-xl mt-2">iv. Types:</p>
+          <p className=" font-semibold text-xl mt-2 transform skew-x-12"> Types:</p>
           <span className=" text-l">
             Available in various body materials, sizes and connection types to
             suit a wide range of applications.
           </span>
         </div>
       </div>
-
+      <br/>
+      <br/>
       <div className=" flex justify-center">
         <hr className=" w-[80%] font-semibold" />
       </div>
 
       {/* ---------- */}
 
-      <div className=" p-2 font-bold text-2xl">
+      <div className="  p-2 font-semibold text-4xl">
         2. Medium Pressure Quick Release Couplers
       </div>
       <div className=" block text-center items-start">
@@ -56,200 +58,166 @@ const AirCylinders = () => {
           alt=""
           className=" float-left  w1/2 me-4 ms-4"
           src={`/2. Medium Pressure Quick Release Couplers.jpg`}
-          height={450}
-          width={450}
+          height={350}
+          width={350}
         ></Image>
         <div className=" text-justify">
-          <p className="font-semibold text-2xl ">Features</p>
-          <p className=" font-semibold text-xl mt-2">
+          <p className="font-semibold text-2xl text-gray-500  ">Features</p>
+          <p className=" font-semibold text-xl mt-2 transform skew-x-12">
             {" "}
-            i. Two-way Directional:
+            Two-way Directional:
           </p>
           <span className=" text-l">
             Two way shut-off with an automatic shut-off valve incorporated into
             both the socket and the plug.
           </span>
-          <p className=" font-semibold text-xl mt-2">ii. Auto Shut-off:</p>
+          <p className=" font-semibold text-xl mt-2 transform skew-x-12"> Auto Shut-off:</p>
           <span className=" text-l">
             Automatic shut-off valves in socket and plug provide an instant
             perfect seal when coupler is disconnected. (No fluid loss or mixing
             of air when connecting or disconnecting the plug).
           </span>
-          <p className=" font-semibold text-xl mt-2">iii. Body Material:</p>
+          <p className=" font-semibold text-xl mt-2 transform skew-x-12"> Body Material:</p>
           <span className=" text-l">
             Variety of body materials. Packing can be selected according to
             fluid type.
           </span>
-          <p className=" font-semibold text-xl mt-2">iv. Technology:</p>
+          <p className=" font-semibold text-xl mt-2 transform skew-x-12"> Technology:</p>
           <span className=" text-l">
             Highly accurate machining technology creates an extremely tight
             seal, designed for a wide variety of applications.
           </span>
         </div>
       </div>
-
+      <br/>
+      <br/>
       <div className=" flex justify-center">
         <hr className=" w-[80%] font-semibold" />
       </div>
       {/* ---------- */}
 
-      <div className=" p-2 font-bold text-2xl">
-        3. Hi-Pressure Quick Release Couplers - Up to 210 Kg/cm²
+      <div className="  p-2 font-semibold text-4xl">
+        3. Hydraulic Quick Release Couplers - Up to 210 Kg/cm²
       </div>
       <div className=" block text-center items-start">
         <Image
           alt=""
           className=" float-left  w1/2 me-4 ms-4"
           src={`/3. Hi-Pressure Quick Release Couplers.jpg`}
-          height={450}
-          width={450}
+          height={350}
+          width={350}
         ></Image>
         <div className=" text-justify">
-          <p className="font-semibold text-2xl ">Features</p>
-          <p className=" font-semibold text-xl mt-2">
+          <p className="font-semibold text-2xl text-gray-500  ">Features</p>
+          <p className=" font-semibold text-xl mt-2 transform skew-x-12">
             {" "}
-            i. Two-way Directional:
+             Two-way Directional:
           </p>
           <span className=" text-l">
             Two way shut-off coupler constructed of a special high-grade steel.
           </span>
-          <p className=" font-semibold text-xl mt-2">ii. Application:</p>
+          <p className=" font-semibold text-xl mt-2 transform skew-x-12"> Application:</p>
           <span className=" text-l">
             Vibration and Impact resistant, together with an extremely low
             pressure loss, makes this coupler ideal for high pressure hydraulic
             systems.
           </span>
-          <p className=" font-semibold text-xl mt-2">iii. Non-Standard Type:</p>
+          <p className=" font-semibold text-xl mt-2 transform skew-x-12"> Non-Standard Type:</p>
           <span className=" text-l">
             Custom cylinders are available as are non-standard strokes, rod
             extension and special rod threads.{" "}
           </span>
-          <p className=" font-semibold text-xl mt-2">iii. Special Threads:</p>
+          <p className=" font-semibold text-xl mt-2 transform skew-x-12"> Special Threads:</p>
           <span className=" text-l">
             Heat treated to resist hydraulic / mechanical shock and specially
             treated threads protect against cracking caused by over tightening.
           </span>
         </div>
       </div>
-
+      <br/>
+      <br/>
       <div className=" flex justify-center">
         <hr className=" w-[80%] font-semibold" />
       </div>
       {/* ---------- */}
-
-      <div className=" p-2 font-bold text-2xl">
-        4. Straight Through Quick Release Couplers
-      </div>
-      <div className=" block text-center items-start">
-        <Image
-          alt=""
-          className=" float-left  w1/2 me-4 ms-4"
-          src={`/4. Straight Through Quick Release Couplers.jpg`}
-          height={450}
-          width={450}
-        ></Image>
-        <div className=" text-justify">
-          <p className="font-semibold text-2xl ">Features</p>
-          <p className=" font-semibold text-xl mt-2"> i. High Flow:</p>
-          <span className=" text-l">
-            Valveless construction drastically saves pressure loss and achieves
-            high flow rate.
-          </span>
-          <p className=" font-semibold text-xl mt-2">ii. Use:</p>
-          <span className=" text-l">
-            Suitable for high viscocity fluids (such as grease).
-          </span>
-          <p className=" font-semibold text-xl mt-2">iii. Types:</p>
-          <span className=" text-l">
-            Available in various standard body materials, sizes and end
-            configurations to cope with diversified applications and operating
-            situations.
-          </span>
-        </div>
-      </div>
-
-      <div className=" flex justify-center">
-        <hr className=" w-[80%] font-semibold" />
-      </div>
-      <br />
-      <br />
-      {/* ---------- */}
-      <div className=" p-2 font-bold text-2xl">
-        5. Mould Type Quick Release Couplers
+      <div className="  p-2 font-semibold text-4xl">
+        4. Mould Type Quick Release Couplers
       </div>
       <div className=" block text-center items-start">
         <Image
           alt=""
           className=" float-left  w1/2 me-4 ms-4"
           src={`/5. Mould Type Quick Release Couplers.jpg`}
-          height={450}
-          width={450}
+          height={350}
+          width={350}
         ></Image>
         <div className=" text-justify">
-          <p className="font-semibold text-2xl ">Features</p>
-          <p className=" font-semibold text-xl mt-2"> i. Plugs & Sockets:</p>
+          <p className="font-semibold text-2xl text-gray-500  ">Features</p>
+          <p className=" font-semibold text-xl mt-2 transform skew-x-12"> Plugs & Sockets:</p>
           <span className=" text-l">
             The plugs and sockets provide quick connect and disconnect method of
             changing heating and cooling hoses to the mold as well as, from
             water manifolds or supply sources.
           </span>
-          <p className=" font-semibold text-xl mt-2">ii. Use:</p>
+          <p className=" font-semibold text-xl mt-2 transform skew-x-12"> Use:</p>
           <span className=" text-l">
             Mold couplers are designed for used with plastic molds and die cast
             dies in water, air or oil lines.
           </span>
-          <p className=" font-semibold text-xl mt-2">iii. Types:</p>
+          <p className=" font-semibold text-xl mt-2 transform skew-x-12"> Types:</p>
           <span className=" text-l">
             Available in combination of brass and stainless steel raw material.
           </span>
-          <p className=" font-semibold text-xl mt-2">iv. Capacity:</p>
+          <p className=" font-semibold text-xl mt-2 transform skew-x-12"> Capacity:</p>
           <span className=" text-l">
             100% leak proof connections with having a maximum rated capacity of
             13 bar and withstand temperatures of up to 200°C.
           </span>
         </div>
       </div>
-
+      <br/>
+      <br/>
       <div className=" flex justify-center">
         <hr className=" w-[80%] font-semibold" />
       </div>
       {/* ---------- */}
-      <div className=" p-2 font-bold text-2xl">
-        6. Camlock Type Quick Release Couplers
+      <div className="  p-2 font-semibold text-4xl">
+        5. Camlock Type Quick Release Couplers
       </div>
       <div className=" block text-center items-start">
         <Image
           alt=""
           className=" float-left  w1/2 me-4 ms-4"
           src={`/6. Camlock Type Quick Release Couplers.jpg`}
-          height={450}
-          width={450}
+          height={350}
+          width={350}
         ></Image>
         <div className=" text-justify">
-          <p className="font-semibold text-2xl ">Features</p>
-          <p className=" font-semibold text-xl mt-2"> i. Applications:</p>
+          <p className="font-semibold text-2xl text-gray-500  ">Features</p>
+          <p className=" font-semibold text-xl mt-2 transform skew-x-12"> Applications:</p>
           <span className=" text-l">
             This coupler complies with diversified applications in liquid or gas
             transportation.
           </span>
-          <p className=" font-semibold text-xl mt-2">ii. Structure:</p>
+          <p className=" font-semibold text-xl mt-2 transform skew-x-12"> Structure:</p>
           <span className=" text-l">
             End-face structure enables no bumps or hollows on the internal fluid
             passage and ensures smooth fluid transportation.
           </span>
-          <p className=" font-semibold text-xl mt-2">iii. Connection:</p>
+          <p className=" font-semibold text-xl mt-2 transform skew-x-12"> Connection:</p>
           <span className=" text-l">
             Light lever pull-down will connect the plug and socket without fail
             ready to flow liquid or gases.
           </span>
-          <p className=" font-semibold text-xl mt-2">iv. Types:</p>
+          <p className=" font-semibold text-xl mt-2 transform skew-x-12"> Types:</p>
           <span className=" text-l">
             The variety of body materials, sizes and end configurations has been
             standardized to comply with wide range of applications.
           </span>
         </div>
       </div>
-
+      <br/>
+      <br/>
       <div className=" flex justify-center">
         <hr className=" w-[80%] font-semibold" />
       </div>
