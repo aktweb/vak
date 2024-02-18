595abfe807263340a8c3067e13b40fac4240b923
aktweb <aktweb.0@gmail.com>
2024-02-18 22:09:42 +0530

    test

--- app/item/pneumatic-series-tubes/page.tsx
+++ app/item/pneumatic-series-tubes/page.tsx
@@ -2,11 +2,12 @@ import Image from "next/image";
 const AirCylinders = () => {
   return (
     <div className=" flex-col space-y-10">
-      <div className=" flex justify-center font-semibold text-xl">
+      <br/>
+      <div className=" flex justify-center font-bold text-5xl text-slate-700">
         Pneumatic Series Tubes
       </div>
 
-      <div className=" p-2 font-bold text-2xl">1. Polyurethane Tubes</div>
+      <div className="  p-2 font-semibold text-3xl">1. Polyurethane Tubes</div>
       <div className=" block text-center items-start">
         <Image
           alt=""
@@ -16,41 +17,42 @@ const AirCylinders = () => {
           width={450}
         ></Image>
         <div className=" text-justify">
-          <p className="font-semibold text-2xl ">Features</p>
-          <p className=" font-semibold text-xl mt-2"> i. Efficiency:</p>
+          <p className="font-semibold text-2xl text-gray-500 ">Features</p>
+          <p className=" font-semibold text-xl mt-2 transform skew-x-12"> Efficiency:</p>
           <span className=" text-l">
             Ultra softness enables to display excellent efficiency in piping
           </span>
-          <p className=" font-semibold text-xl mt-2">ii. Properties:</p>
+          <p className=" font-semibold text-xl mt-2 transform skew-x-12">Properties:</p>
           <span className=" text-l">
             Outstanding anti-weather, anti-curve and anti-fatigue properties
           </span>
-          <p className=" font-semibold text-xl mt-2">
-            iii. Available ID VPU Series:
+          <p className=" font-semibold text-xl mt-2 transform skew-x-12">
+             Available ID VPU Series:
           </p>
           <span className=" text-l">2.5, 4, 6, 8, 9, 10 and 12mm</span>
-          <p className=" font-semibold text-xl mt-2">
-            iv. Available OD PNU Series:
+          <p className=" font-semibold text-xl mt-2 transform skew-x-12">
+            Available OD PNU Series:
           </p>
           <span className=" text-l">4, 6, 8, 9, 10, 12, 14 and 16mm</span>
-          <p className=" font-semibold text-xl mt-2">v. Available Colours:</p>
+          <p className=" font-semibold text-xl mt-2 transform skew-x-12"> Available Colours:</p>
           <span className=" text-l">
             Blue, Yellow, Black, Red, Green, Transparent and Milky White
           </span>
-          <p className=" font-semibold text-xl mt-2">vi. Customized Colours:</p>
+          <p className=" font-semibold text-xl mt-2 transform skew-x-12"> Customized Colours:</p>
           <span className=" text-l">
             Customized Colours available on request with minimum order quantity
           </span>
         </div>
       </div>
-
+      <br/>
+      <br/>
       <div className=" flex justify-center">
         <hr className=" w-[80%] font-semibold" />
       </div>
 
       {/* ---------- */}
 
-      <div className=" p-2 font-bold text-2xl">2. Nylon Tubes</div>
+      <div className="  p-2 font-semibold text-3xl">2. Nylon Tubes</div>
       <div className=" block text-center items-start">
         <Image
           alt=""
@@ -60,40 +62,41 @@ const AirCylinders = () => {
           width={450}
         ></Image>
         <div className=" text-justify">
-          <p className="font-semibold text-2xl ">Features</p>
-          <p className=" font-semibold text-xl mt-2"> i. Applications:</p>
+          <p className="font-semibold text-2xl text-gray-500 ">Features</p>
+          <p className=" font-semibold text-xl mt-2 transform skew-x-12"> Applications:</p>
           <span className=" text-l">
             Applicable for multi-purpose piping for Air, Water, Oil & Chemical
           </span>
-          <p className=" font-semibold text-xl mt-2">ii. Features:</p>
+          <p className=" font-semibold text-xl mt-2 transform skew-x-12"> Features:</p>
           <span className=" text-l">
             Superior, anti-pressure, anti-vibration, anti-rust, anti-chemical
             and anti-oil
           </span>
-          <p className=" font-semibold text-xl mt-2">iii. Productivity:</p>
+          <p className=" font-semibold text-xl mt-2 transform skew-x-12"> Productivity:</p>
           <span className=" text-l">
             Light weight, and softness, enables to increase the productivity of
             piping works
           </span>
-          <p className=" font-semibold text-xl mt-2">iv. Available ID:</p>
+          <p className=" font-semibold text-xl mt-2 transform skew-x-12">Available ID:</p>
           <span className=" text-l">2, 2.5, 4, 4.35, 6, 8, 10 and 12mm</span>
-          <p className=" font-semibold text-xl mt-2">v. Available OD:</p>
+          <p className=" font-semibold text-xl mt-2 transform skew-x-12">Available OD:</p>
           <span className=" text-l">4, 5, 6, 8, 10, 12, 14 and 16mm</span>
-          <p className=" font-semibold text-xl mt-2">vi. Standard Colours:</p>
+          <p className=" font-semibold text-xl mt-2 transform skew-x-12"> Standard Colours:</p>
           <span className=" text-l">Natural White and Black</span>
-          <p className=" font-semibold text-xl mt-2">vi. Customized Colours:</p>
+          <p className=" font-semibold text-xl mt-2 transform skew-x-12"> Customized Colours:</p>
           <span className=" text-l">
             Customized Colours available on request with minimum order quantity
           </span>
         </div>
       </div>
-
+      <br/>
+      <br/>
       <div className=" flex justify-center">
         <hr className=" w-[80%] font-semibold" />
       </div>
       {/* ---------- */}
 
-      <div className=" p-2 font-bold text-2xl">
+      <div className="  p-2 font-semibold text-3xl">
         3. Anti-Spatter Polyurethane Tubes
       </div>
       <div className=" block text-center items-start">
@@ -105,34 +108,35 @@ const AirCylinders = () => {
           width={450}
         ></Image>
         <div className=" text-justify">
-          <p className="font-semibold text-2xl ">Features</p>
-          <p className=" font-semibold text-xl mt-2">i. Efficiency:</p>
+          <p className="font-semibold text-2xl text-gray-500 ">Features</p>
+          <p className=" font-semibold text-xl mt-2 transform skew-x-12">Efficiency:</p>
           <span className=" text-l">
             Ultra softness enables to display excellent efficiency in piping
           </span>
-          <p className=" font-semibold text-xl mt-2">ii. Properties:</p>
+          <p className=" font-semibold text-xl mt-2 transform skew-x-12">Properties:</p>
           <span className=" text-l">
             Outstanding anti-weather, anti-curve and anti-fatigue properties
           </span>
-          <p className=" font-semibold text-xl mt-2">iii. Applications:</p>
+          <p className=" font-semibold text-xl mt-2 transform skew-x-12"> Applications:</p>
           <span className=" text-l">Suitable for welding applications</span>
-          <p className=" font-semibold text-xl mt-2">iv. Available OD:</p>
+          <p className=" font-semibold text-xl mt-2 transform skew-x-12">Available OD:</p>
           <span className=" text-l">6, 8, 10 and 12mm</span>
-          <p className=" font-semibold text-xl mt-2">v. Available Colours:</p>
+          <p className=" font-semibold text-xl mt-2 transform skew-x-12">Available Colours:</p>
           <span className=" text-l">
             Blue, Yellow, Black, Red, Green and White
           </span>
-          <p className=" font-semibold text-xl mt-2">vi. Customized Colours:</p>
+          <p className=" font-semibold text-xl mt-2 transform skew-x-12">Customized Colours:</p>
           <span className=" text-l">
             Customized Colours available on request with minimum order quantity
           </span>
         </div>
       </div>
-
+      <br/>
+      <br/>
       <div className=" flex justify-center">
         <hr className=" w-[80%] font-semibold" />
       </div>
-      <div className=" p-2 font-bold text-2xl">
+      <div className="  p-2 font-semibold text-3xl">
         4. Polyurethane Coiled Hoses
       </div>
       <div className=" block text-center items-start">
@@ -144,43 +148,44 @@ const AirCylinders = () => {
           width={450}
         ></Image>
         <div className=" text-justify">
-          <p className="font-semibold text-2xl ">Features</p>
-          <p className=" font-semibold text-xl mt-2">i. Compact Size:</p>
+          <p className="font-semibold text-2xl text-gray-500 ">Features</p>
+          <p className=" font-semibold text-xl mt-2 transform skew-x-12">Compact Size:</p>
           <span className=" text-l">
             Easy to use where space is restricted due to highly compact diameter
             of recoil
           </span>
-          <p className=" font-semibold text-xl mt-2">ii. Good Resistant:</p>
+          <p className=" font-semibold text-xl mt-2 transform skew-x-12"> Good Resistant:</p>
           <span className=" text-l">Good resistance to shock and abrasion</span>
-          <p className=" font-semibold text-xl mt-2">iii. Fittings:</p>
+          <p className=" font-semibold text-xl mt-2 transform skew-x-12">Fittings:</p>
           <span className=" text-l">
             Full flow due to straight ends equipped with different types of
             fittings.
           </span>
-          <p className=" font-semibold text-xl mt-2">iv. Tube Sizes:</p>
+          <p className=" font-semibold text-xl mt-2 transform skew-x-12">Tube Sizes:</p>
           <span className=" text-l">
             4, 6, 8, 10, 12, 14 and 16mm OD tubing&apos;s
           </span>
-          <p className=" font-semibold text-xl mt-2">
-            v. Available Working Length (Mtrs):
+          <p className=" font-semibold text-xl mt-2 transform skew-x-12">
+          Available Working Length (Mtrs):
           </p>
           <span className=" text-l">
             1.5, 2, 3, 4, 5, 6, 8, 10, 12, 15 and 20
           </span>
-          <p className=" font-semibold text-xl mt-2">vi. Standard Colours:</p>
+          <p className=" font-semibold text-xl mt-2 transform skew-x-12">Standard Colours:</p>
           <span className=" text-l">Blue & Yellow</span>
-          <p className=" font-semibold text-xl mt-2">vii. Customized Coil:</p>
+          <p className=" font-semibold text-xl mt-2 transform skew-x-12">Customized Coil:</p>
           <span className=" text-l">
             Customized Coil Dia and Colours available on request with minimum
             order quantity.
           </span>
         </div>
       </div>
-
+      <br/>
+      <br/>
       <div className=" flex justify-center">
         <hr className=" w-[80%] font-semibold" />
       </div>
-      <div className=" p-2 font-bold text-2xl">5. Nylon Coiled Hoses</div>
+      <div className="  p-2 font-semibold text-3xl">5. Nylon Coiled Hoses</div>
       <div className=" block text-center items-start">
         <Image
           alt=""
@@ -190,37 +195,38 @@ const AirCylinders = () => {
           width={450}
         ></Image>
         <div className=" text-justify">
-          <p className="font-semibold text-2xl ">Features</p>
-          <p className=" font-semibold text-xl mt-2">i. Compact Size:</p>
+          <p className="font-semibold text-2xl text-gray-500 ">Features</p>
+          <p className=" font-semibold text-xl mt-2 transform skew-x-12">Compact Size:</p>
           <span className=" text-l">
             Easy to use where space is restricted due to highly compact diameter
             of recoil.
           </span>
-          <p className=" font-semibold text-xl mt-2">ii. Good Resistant:</p>
+          <p className=" font-semibold text-xl mt-2 transform skew-x-12">Good Resistant:</p>
           <span className=" text-l">Good resistance to shock and abrasion</span>
-          <p className=" font-semibold text-xl mt-2">iii. Fittings:</p>
+          <p className=" font-semibold text-xl mt-2 transform skew-x-12">Fittings:</p>
           <span className=" text-l">
             Full flow due to straight ends equipped with different types of
             fittings.
           </span>
-          <p className=" font-semibold text-xl mt-2">iv. Tube Sizes:</p>
+          <p className=" font-semibold text-xl mt-2 transform skew-x-12">Tube Sizes:</p>
           <span className=" text-l">
             4, 6, 8, 10, 12, 14 and 16mm OD tubing&apos;s
           </span>
-          <p className=" font-semibold text-xl mt-2">
-            v. Available Working Length (Mtrs):
+          <p className=" font-semibold text-xl mt-2 transform skew-x-12">
+          Available Working Length (Mtrs):
           </p>
           <span className=" text-l">3, 4, 5, 6, 8, 10 and 12</span>
-          <p className=" font-semibold text-xl mt-2">vi. Standard Colours:</p>
+          <p className=" font-semibold text-xl mt-2 transform skew-x-12">Standard Colours:</p>
           <span className=" text-l">Blue and Yellow</span>
-          <p className=" font-semibold text-xl mt-2">vii. Customized Coil:</p>
+          <p className=" font-semibold text-xl mt-2 transform skew-x-12">Customized Coil:</p>
           <span className=" text-l">
             Customized Coil Dia and Colours available on request with minimum
             order quantity.
           </span>
         </div>
       </div>
-
+      <br/>
+      <br/>
       <div className=" flex justify-center">
         <hr className=" w-[80%] font-semibold" />
       </div>
